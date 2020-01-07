0ad854050fb07210a11f9d4f44eecc69383c3a4c
Nishant Mahajan <nishant.mahajanXVII@gmail.com>
2020-01-07 20:35:02 +0530

    Add a file to hold demo-wide constants

---
+++ src/conf.js
@@ -0,0 +1,6 @@
+import { Vec2 } from "./vec2.js";
+
+export const cameraVel = 4;
+export const flapForce = 7;
+export const gravity = 0.7;
+export const worldBounds = new Vec2(window.innerWidth / 2, window.innerHeight / 2);
