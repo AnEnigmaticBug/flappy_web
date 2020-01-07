3a5b595f128beb7cf8a10e2df503b2465cae9bc1
Nishant Mahajan <nishant.mahajanXVII@gmail.com>
2020-01-07 20:36:11 +0530

    Add code for generating pipe pairs in the game

--- src/pipe-pair.js
+++ src/pipe-pair.js
@@ -1,6 +1,33 @@
+import { Vec2 } from "./vec2.js";
+
 export class PipePair {
     constructor(gapPos, gapBounds) {
         this.gapPos = gapPos;
         this.gapBounds = gapBounds;
+    }
+}
+
+export class PipePairGenerator {
+    constructor(worldBounds) {
+        this.worldBounds = worldBounds;
+        this.n = 0;
+        this.randomization = Math.random();
+    }
+
+    test() {
+        return new PipePair(
+            new Vec2(240 * this.n, (this.worldBounds.y / 2) - 60 + Math.sin(this.n * 2 + this.randomization) * 60),
+            new Vec2(60, 120),
+        );
+    }
+
+    next() {
+        const p = this.test();
+        this.n++;
+        return p;
+    }
+
+    fastForwardTo(x) {
+        while (this.next().gapPos.x < x) { }
     }
 }
