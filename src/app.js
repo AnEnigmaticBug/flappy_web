9db4f92f7cd87902aa97f011374b4526d45f0bec
Nishant Mahajan <nishant.mahajanXVII@gmail.com>
2020-05-04 22:54:32 +0530

    Modify app.js to show the GA demo instead of the game

--- src/app.js
+++ src/app.js
@@ -1,31 +1,9 @@
-import { Bird } from './bird.js';
 import { worldBounds } from './conf.js';
-import { KeyController } from './controller.js';
-import { Game } from './game.js';
-import { PipePairGenerator } from './pipe-pair.js';
 import { Renderer } from './renderer.js';
 import { resizeCanvas } from './util.js';
-import { Vec2 } from './vec2.js';
+import { Evolver } from './evolver.js';
 
 resizeCanvas(worldBounds);
 
-const game = new Game(
-    [
-        new Bird(new Vec2(34, 24), new KeyController(32)),
-    ],
-    new PipePairGenerator(worldBounds),
-    worldBounds,
-);
-
 const renderer = new Renderer(document.getElementById('canvas').getContext('2d'));
-
-const loop = () => {
-    if (game.isOver()) {
-        return;
-    }
-    game.update();
-    renderer.render(game);
-    requestAnimationFrame(loop);
-};
-
-loop();
+new Evolver(200).run(renderer, 32);
