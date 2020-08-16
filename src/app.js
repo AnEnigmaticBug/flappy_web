e5aec3bcbeac0b34d2efa1f26e2c1363f3fb3d70
Nishant Mahajan <nishant.mahajanXVII@gmail.com>
2020-08-16 14:43:19 +0530

    Add basic GUI for menu, exit and play screens
    The menu screen only has 1 option right now; of starting the evolution.
    It'll be expanded to allow the user to choose between playing a game or
    evolving the birds.

--- src/app.js
+++ src/app.js
@@ -5,5 +5,25 @@ import { Evolver } from './evolver.js';
 
 resizeCanvas(worldBounds);
 
-const renderer = new Renderer(document.getElementById('canvas').getContext('2d'));
-new Evolver(200).run(renderer, 32);
+const menuScreen = document.getElementById('menu-screen');
+const playScreen = document.getElementById('play-screen');
+const exitScreen = document.getElementById('exit-screen');
+
+playScreen.style.display = 'none';
+exitScreen.style.display = 'none';
+
+document.getElementById('play-button').addEventListener('click', async (ev) => {
+    menuScreen.style.display = 'none';
+    playScreen.style.display = 'flex';
+
+    const renderer = new Renderer(document.getElementById('canvas').getContext('2d'));
+    await new Evolver(200).run(renderer, 32);
+
+    playScreen.style.display = 'none';
+    exitScreen.style.display = 'flex';
+});
+
+document.getElementById('okay-button').addEventListener('click', (ev) => {
+    exitScreen.style.display = 'none';
+    menuScreen.style.display = 'flex';
+});
--- src/conf.js
+++ src/conf.js
@@ -3,4 +3,4 @@ import { Vec2 } from "./vec2.js";
 export const cameraVel = 4;
 export const flapForce = 7;
 export const gravity = 0.7;
-export const worldBounds = new Vec2(window.innerWidth / 2, window.innerHeight / 2);
+export const worldBounds = new Vec2(640, 360);
