5a08e89a01fc0f0459efe52c0e92c87145f4f13f
Nishant Mahajan <nishant.mahajanXVII@gmail.com>
2024-09-10 18:17:27 +0530

    Add a button to end evolution any time

--- src/evolve-screen.js
+++ src/evolve-screen.js
@@ -10,13 +10,19 @@ const scoreIncrementer = new ScoreIncrementer(document.getElementById('score'));
 const renderer = new Renderer(document.getElementById('canvas').getContext('2d'));
 const populationSize = 200;
 sessionStorage.setItem('populationSize', populationSize);
+
+const onFinish = () => {
+    scoreIncrementer.stop();
+    sessionStorage.setItem('maxScore', scoreIncrementer.maxScore);
+    window.location.href = 'result.html';
+};
+document.getElementById('end-button').addEventListener('click', onFinish);
+
 await new Evolver(populationSize).run(renderer, worldBounds, 32, (generation) => {
     document.getElementById('generation').innerText = generation.toString();
     scoreIncrementer.reset();
     sessionStorage.setItem('generationCount', generation + 1);
 });
 
-scoreIncrementer.stop();
-sessionStorage.setItem('maxScore', scoreIncrementer.maxScore);
 // Evolver.run finishes only when all generations are simulated.
-window.location.href = 'result.html';
+onFinish();
